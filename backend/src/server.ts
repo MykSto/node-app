08f1539817bede42288abe89bab547220098e754
MykSto <hushokas@gmail.com>
2025-07-02 18:48:15 +0200

    feat(db): connect mongodb

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,25 @@
 import app from './app'
 import http from 'http'
 import data from './routes/planets/data'
+import mongoose from 'mongoose'
 
 const PORT = 9000
 
 const server = http.createServer(app)
 
+const MONGO_URL = `mongodb+srv://hushokas-api:${process.env.MONOG_PASS}@cluster0.ciaep03.mongodb.net/nasa?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.connection.once('open', () => {
+  console.log('MongoDB is ready')
+})
+
+mongoose.connection.on('error', err => {
+  console.error('MongoDB error', err)
+})
+
 async function startServer () {
-  // TODO: should it be here?
+  await mongoose.connect(MONGO_URL)
+
   await data.loadPlanets()
 
   server.listen(PORT, () => {
