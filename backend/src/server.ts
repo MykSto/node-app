9dda45921227df0559890be88cfc4ea9b721752c
MykSto <hushokas@gmail.com>
2025-07-21 11:59:45 +0300

    feat(cert): shift to https connection

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,19 @@
+import fs from 'fs'
 import app from './app'
-import http from 'http'
+import https from 'https'
 import { loadPlanets } from './routes/planets/data'
 import { mongooseConnect } from './services/mongoose'
 import config from 'config'
 import { getSpacexLaunches } from './routes/launches/data'
+import path from 'path'
 
 const PORT = config.get('local.be.port')
+const certPath = path.join(__dirname, '../../certs/')
 
-const server = http.createServer(app)
+const server = https.createServer({
+  key: fs.readFileSync(`${certPath}key.pem`),
+  cert: fs.readFileSync(`${certPath}cert.pem`)
+}, app)
 
 async function startServer () {
   await mongooseConnect()
